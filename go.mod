7dda388d6a85d82f8764dce406b98af9d50069f9
githmi-paranahewa <githmiph.20@itfac.mrt.ac.lk>
2024-01-31 19:24:31 +0530

    configure Oauth2

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/order_Service
 
 go 1.21.6
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require golang.org/x/oauth2 v0.16.0
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"time"
+
+	// "os"
+
 	// "fmt"
 	"log"
 	"math/rand"
@@ -9,6 +15,8 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+	// "golang.org/x/oauth2/clientcredentials"
 	// "golang.org/x/tools/go/analysis/passes/appends"
 )
 
@@ -31,6 +39,12 @@ type Order struct {
 	Total  float64
 	Status string
 }
+
+// var clientCredsConfig = clientcredentials.Config{
+// 	ClientID:     "CLIENT_ID",
+// 	ClientSecret: "CLIENT_SECRET",
+// 	TokenURL:     "TOKEN_URL",
+// }
 
 var orders []Order
 
@@ -90,6 +104,45 @@ func UpdateOrder(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
+	// client := clientCredsConfig.Client(context.Background())
+	// // os.Setenv("ServiceURL", "SERVICE_URL")
+	// serviceURL := os.Getenv("ServiceURL")
+	ctx := context.Background()
+
+	conf := &oauth2.Config{
+		ClientID:     "CLIENT_ID",
+		ClientSecret: "CLIENT_SECRET",
+
+		// Scopes: []string{"SCOPE1", "SCOPE2"},
+		Endpoint: oauth2.Endpoint{
+			// AuthURL:  "",
+			TokenURL: "TOKEN_URL",
+		},
+	}
+	// verifier := oauth2.GenerateVerifier()
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use the custom HTTP client when requesting a token.
+	httpClient := &http.Client{Timeout: 2 * time.Second}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+
+	tok, err := conf.Exchange(ctx, code)
+	fmt.Printf("HHH")
+	if err != nil {
+		fmt.Printf("KKK")
+		log.Fatal(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	_ = client
+
+	// rootRouter := r.PathPrefix("/").Subrouter()
+	// rootRouter.Use(authenticateMiddlewaretest)
+
 	orders = append(orders, Order{ID: "1", Items: []OrderItem{{ItemID: "1", Quantity: 2}}, Total: 2000, Status: "Ongoing"})
 	orders = append(orders, Order{ID: "2", Items: []OrderItem{{ItemID: "1", Quantity: 2}, {ItemID: "2", Quantity: 3}}, Total: 2000, Status: "Ongoing"})
 	r.HandleFunc("/order", AddOrder).Methods("POST")
@@ -97,6 +150,61 @@ func main() {
 	r.HandleFunc("/order/{orderId}", GetOrderById).Methods("GET")
 	r.HandleFunc("/order/{orderId}", UpdateOrder).Methods("PUT")
 	r.HandleFunc("/order/{orderId}", DeleteOrder).Methods("DELETE")
+	// http.Handle("/item", authenticateMiddleware(client, serviceURL)(r))
 	log.Fatal(http.ListenAndServe(":9090", r))
 
 }
+
+// func authenticateMiddleware(client *http.Client, serviceURL string) func(http.Handler) http.Handler {
+// 	return func(next http.Handler) http.Handler {
+// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+// 			params := mux.Vars(r)
+// 			a, err := client.Get(params[serviceURL])
+
+// 			if err != nil {
+// 				fmt.Println("url", a, "error", err)
+// 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+// 				return
+// 			}
+
+// 			next.ServeHTTP(w, r)
+// 		})
+// 	}
+// }
+
+// func authenticateMiddleware(next *http.Client, se string) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("middleware2")
+// 		// next.ServeHTTP(w, r)
+// 	})
+// }
+
+// func authenticateMiddlewaretest(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("middleware2")
+// 		_, err := GetOAuth2Token()
+// 		if err != nil {
+// 			fmt.Println("Error obtaining token:", err)
+// 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+// 			return
+// 		}
+// 		// client := clientCredsConfig.Client(context.Background())
+// 		// os.Setenv("ServiceURL", "SERVICE_URL")
+// 		// serviceURL := os.Getenv("ServiceURL")
+// 		// h, err := os.LookupEnv(e)
+// 		// a, err := client.Get(serviceURL)
+// 		// if err != nil {
+// 		// 	fmt.Println("url", a, "error", err)
+// 		// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+// 		// 	return
+// 		// }
+
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
+// func GetOAuth2Token() (*http.Client, error) {
+// 	// Get the OAuth2 token using the client credentials
+// 	client := clientCredsConfig.Client(context.Background())
+// 	return client, nil
+// }
